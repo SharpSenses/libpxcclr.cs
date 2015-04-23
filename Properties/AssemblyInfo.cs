f8737001b3357c8a547cbe8892ff2e27fbe9e4d8
Andre Carlucci <andrecarlucci@gmail.com>
2015-04-22 22:30:50 -0300

    nuspec

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,11 +3,10 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: CLSCompliant(true)]
-[assembly: AssemblyCompany("Intel Corporation")]
+[assembly: AssemblyCompany("Andre Carlucci")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTitle("Intel(R) RealSense(TM) SDK C# Wrapper Library")]
-[assembly: AssemblyDescription("libpxcclr.cs")]
-[assembly: AssemblyProduct("Intel(R) RealSense(TM) SDK")]
-[assembly: AssemblyCopyright("Copyright(C) 2010-2014, Intel Corporation. All Rights Reserved.")]
+[assembly: AssemblyDescription("AnyCpu version of libpxcclr.cs")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("b51e736a-bcfc-4d3d-b2a6-9abc50c783c4")]
